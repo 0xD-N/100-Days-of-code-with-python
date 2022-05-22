880f171380e24337aede01660297a52f4f7aae7e
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-05-22 18:04:00 -0500

    exercise 4 completion

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/Excercises/exercise 4 - variables.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+
+temp = a # using temporary variable to hold the current value of a before changing a's actual value
+a = b;
+b = temp
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
