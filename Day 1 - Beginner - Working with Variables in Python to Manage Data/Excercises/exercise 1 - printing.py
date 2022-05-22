247f7992c3038cc54e46d52005da9624f36dfc77
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-05-22 17:59:01 -0500

    added comments

--- Day 1 - Beginner - Working with Variables in Python to Manage Data/Excercises/exercise 1 - printing.py
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/Excercises/exercise 1 - printing.py
@@ -1,3 +1,5 @@
+#simple exercise of printing output to the screen
+
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
--- Day 1 - Beginner - Working with Variables in Python to Manage Data/Excercises/exercise 2 - Debugging Practice.py
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/Excercises/exercise 2 - Debugging Practice.py
@@ -1,7 +1,19 @@
-#Fix the code below 👇
+#Debugging practice.
 
+#Fixed code:
 print("Day 1 - String Manipulation")
 print("String Concatenation is done with the \"+\" sign.")
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 
+#original broken code:
+'''
+#Fix the code below 👇
+
+print(Day 1 - String Manipulation")                        <-- missing a quote
+print("String Concatenation is done with the "+" sign.")   <-- missing escape sequence for quotes
+  print('e.g. print("Hello " + "world")')                  <-- uneccessary indentation
+print(("New lines can be created with a backslash and n.") <-- uneccessary extra open parenthesis 
+
+
+'''
