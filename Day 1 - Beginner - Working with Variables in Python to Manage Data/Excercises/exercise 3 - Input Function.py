45d2a24f3e11e7094271813711b3630cbeb67f69
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-05-22 17:54:25 -0500

    exercise 3 completion

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/Excercises/exercise 3 - Input Function.py
@@ -0,0 +1,9 @@
+
+#get name from user
+name = input("What is your name?")
+
+#output length of name to console
+print(len(name))
+
+#for my pythonists out there:
+#print(len(input("What is your name?")))
