98ab0b60ca905398616aa84d5d0983664a5dbc1c
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-05-22 20:16:06 -0500

    tip calculator completion

---
+++ Day 2 - Understanding Data Types and How to Manipulate Strings/Final day 2 project/tip calculator.py
@@ -0,0 +1,19 @@
+print("Welcome to the tip calculator.")
+
+# taking user input and casting into proper numbers
+billTotal = float(input("\nWhat was the total bill? $"))
+
+tipPercentage = int(input("\nWhat percentage tip would you like to give? "))
+
+splitAmount = int(input("\nHow many people are you splitting with? "))
+
+#calculating tip
+tip = (float(tipPercentage)/100) * billTotal
+
+#calculating cost per person with tip included
+totalWithTip = billTotal + tip
+
+costPerPerson = totalWithTip / splitAmount
+
+#outputting result
+print(f"\nEach person should pay ${round(costPerPerson, 2)}")
