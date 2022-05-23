926ac9efae343cc22f8101e4b05cb5ba3dff3a75
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-05-22 19:51:57 -0500

    exercise 3 completion

---
+++ Day 2 - Understanding Data Types and How to Manipulate Strings/Excercises/exercise 3 - life in weeks.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+# age is now a integer and no longer a string
+age = int(age)
+
+#90 years to days, weeks, months 
+totaldays90 = 90 * 365
+totalWeeks90 = 90 * 52
+totalMonths90 = 90 * 12
+
+#user's age to days, weeks, months
+userAgeToDays = age * 365
+userAgeToWeeks = age * 52
+userAgeToMonths = age * 12
+
+#output's their time remaining until 90. (Pretty scary)
+print(f"You have {totaldays90 - userAgeToDays} days, {totalWeeks90 - userAgeToWeeks} weeks, and {totalMonths90 - userAgeToMonths} months left.")
+
+
