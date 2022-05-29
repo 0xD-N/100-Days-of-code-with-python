4ce8eb18aa9e3c263521dbadfeb40c3ebb27b844
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-05-28 23:43:56 -0500

    exercise 3 completion

---
+++ Day 4 - Randomisation and Python Lists/exercises/exercise 3 - Treasure Map.py
@@ -0,0 +1,25 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+'''
+Example Input:
+column 2, row 3 would be entered as:
+
+23
+'''
+
+#take each character and cast it to an integer and then mark an x at the specific row and column
+map[int(position[1]) - 1][int(position[0]) - 1] = "X"
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
