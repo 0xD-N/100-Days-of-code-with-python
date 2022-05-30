0d356a78d420082ddc0ef898a8a6174b05279389
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-05-29 20:29:35 -0500

    exercise 3 - completion

---
+++ Day 5 - Python Loops/exercises/exercise 3 - adding even numbers.py
@@ -0,0 +1,10 @@
+#freestyling :))))
+
+
+#this example is for my pythonists out there
+
+
+# this method uses the built in sum function along with list comprehension to make this possible in one line. 
+
+# To understand this more, research on list comprehension in python.
+print(sum([x for x in range(101) if x % 2 == 0]))
