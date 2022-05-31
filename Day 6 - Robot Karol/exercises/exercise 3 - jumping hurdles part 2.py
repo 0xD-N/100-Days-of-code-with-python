ec8707f62814543996bb7cc098bce35ffb5457df
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-05-30 21:15:31 -0500

    exercise 3 completion

---
+++ Day 6 - Robot Karol/exercises/exercise 3 - jumping hurdles part 2.py
@@ -0,0 +1,22 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+def turn_right():
+    
+    turn_left()
+    turn_left()
+    turn_left()
+
+#assumes bot is looking at hurdle 
+def jump():
+    
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    move()
+    jump()
