8eee669c7c882cd051ead75878e91b9dfef3fe74
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-05-30 22:38:27 -0500

    final day project complete

---
+++ Day 6 - Robot Karol/final day project/maze.py
@@ -0,0 +1,21 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# move, move, left, move, move, right
+
+def turn_right():
+    
+    turn_left()
+    turn_left()
+    turn_left()
+    
+    
+while not at_goal():
+    
+    if right_is_clear():
+        
+        turn_right()
+        move()
+    else:
+        turn_left()
+        
+        
