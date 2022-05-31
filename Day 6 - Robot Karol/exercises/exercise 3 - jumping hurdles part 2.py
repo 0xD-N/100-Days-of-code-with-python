4cc209b2fae15047c0e063ee2daab7e88d70b9c5
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-05-30 21:16:00 -0500

    Update exercise 3 - jumping hurdles part 2.py

--- Day 6 - Robot Karol/exercises/exercise 3 - jumping hurdles part 2.py
+++ Day 6 - Robot Karol/exercises/exercise 3 - jumping hurdles part 2.py
@@ -1,4 +1,4 @@
-#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
 
 def turn_right():
     
