fa7aa9d74609ed22f421625e58c7a7dd2284f6dc
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-06-04 21:03:42 -0500

    exercise completion

---
+++ Day 12 - Debugging/exercises/exercise 2 - Debugging leap year.py
@@ -0,0 +1,17 @@
+# original line did not cast the input into an integer, remaining a string
+
+# year = input("Which year do you want to check?")
+
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+
---
+++ Day 12 - Debugging/exercises/exercise 3 - Debugging FizzBuzz.py
@@ -0,0 +1,30 @@
+
+#original code:
+
+# for number in range(1, 101):
+    
+#   if number % 3 == 0 or number % 5 == 0:
+#     print("FizzBuzz")
+#   if number % 3 == 0:
+#     print("Fizz")
+#   if number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print([number])
+
+
+# fixed code (fixed if statements, and uneccessary brackets)
+
+for number in range(1, 101):
+    
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
+    
+
+
