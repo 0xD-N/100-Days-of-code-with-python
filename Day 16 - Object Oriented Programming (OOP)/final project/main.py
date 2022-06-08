9cf15abd570e41054e9f95a7c6e8e5fdec19b99b
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-06-07 22:07:21 -0500

    cash machine part 2 completion

--- Day 16 - Object Oriented Programming (OOP)/final project/main.py
+++ Day 16 - Object Oriented Programming (OOP)/final project/main.py
@@ -1,3 +1,31 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+machine = CoffeeMaker()
+menu = Menu()
+CashMachine = MoneyMachine()
+
+user_input = ""
+
+while user_input != "off":
+    
+    user_input = input("\nWhat would you like? (espresso/latte/cappuccino): ").lower()
+    
+    if user_input == "off":
+        break
+    elif user_input == "report":
+        machine.report()
+        print(f"cash: ${CashMachine.profit}")
+    else:
+        selection = menu.find_drink(user_input)
+        
+        if(selection):
+            
+            if machine.is_resource_sufficient(selection):            
+                
+                if(CashMachine.make_payment(selection.cost)):
+                    machine.make_coffee(selection)
+                
+                
+            
