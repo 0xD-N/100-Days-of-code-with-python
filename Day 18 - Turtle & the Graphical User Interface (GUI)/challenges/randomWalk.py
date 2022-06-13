d62dc1894c39a4c0c4fbba1e77147d8c11afac91
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-06-13 00:07:41 -0500

    completion

--- Day 18 - Turtle & the Graphical User Interface (GUI)/challenges/randomWalk.py
+++ Day 18 - Turtle & the Graphical User Interface (GUI)/challenges/randomWalk.py
@@ -5,6 +5,8 @@ from turtle import Turtle, Screen
 t = Turtle()
 t.shape("turtle")
 t.color("aquamarine4")
+s = Screen()
+s.colormode(255)
 
 t.pensize(15)
 t.speed(10)
@@ -13,7 +15,7 @@ angles = [90, 180, 270, 0]
 
 def randomPath(angle, length):
     
-    t.pencolor(random.random(), random.random(), random.random())
+    t.pencolor(int(1 + random.random() * 255), int(1 + random.random() * 255), int(1 + random.random() * 255))
     t.setheading(angle)
     t.forward(length)
     
@@ -21,6 +23,6 @@ def randomPath(angle, length):
 for _ in range(200):
     
     randomPath(random.choice(angles), 25)
-    
-s = Screen()
+
+
 s.exitonclick()
